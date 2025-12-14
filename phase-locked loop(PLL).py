8273a39af3c4a23cb48e52272a94a9fecbb95c5a
Velya-Sb <vladislavupir@gmail.com>
2025-12-14 20:31:28 +0300

    Update phase-locked loop(PLL).py

--- phase-locked loop(PLL).py
+++ phase-locked loop(PLL).py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 from math import sin
-xvals = [[], []]
+xvals = [[], [], []]
 alpha = 0.0
 endd = 4.0
 start = -3.0
@@ -19,14 +19,17 @@ while(start < 3.0):
             xold = xnew
             xvals[0].append(alpha)
             xvals[1].append(xnew)
+            xvals[2].append(4 - alpha)
             if(abs(xnew - xss) < 0.001):
                 break
         alpha += 0.001
     start += 0.1
 fig, ax = plt.subplots(facecolor='black')
 ax.set_facecolor('black')
-ax.plot(xvals[0], xvals[1],'.', markersize = 1.2, color = 'white', linewidth=0.1)
+ax.plot(xvals[1], xvals[2],'.', markersize = 1.2, color = 'white', linewidth=0.1)
 ax.tick_params(axis='both', colors = 'white')
+ax.set_yticks([0, 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4])
+ax.set_yticklabels(['4', '3.5', '3', '2.5', '2', '1.5', '1', '0.5', '0'])
 ax.spines['bottom'].set_color('white')
 ax.spines['top'].set_color('white')
 ax.spines['left'].set_color('white')
